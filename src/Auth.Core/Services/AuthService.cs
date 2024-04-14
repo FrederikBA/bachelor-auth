8da32b228892cb83f465e60e320bf28c77db2725
FrederikBA <frederikbandersen98@gmail.com>
2024-04-14 12:13:53 +0200

    dependencey inject user repository in service

--- src/Auth.Core/Services/AuthService.cs
+++ src/Auth.Core/Services/AuthService.cs
@@ -1,11 +1,19 @@
 using Auth.Core.Entities;
 using Auth.Core.Interfaces.DomainServices;
+using Auth.Core.Interfaces.Repositories;
 using Auth.Core.Models.Dtos;
 
 namespace Auth.Core.Services;
 
 public class AuthService : IAuthService
 {
+    private readonly IRepository<User> _userRepository;
+
+    public AuthService(IRepository<User> userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
     public Task<string> LoginAsync(LoginDto dto)
     {
         throw new NotImplementedException();
